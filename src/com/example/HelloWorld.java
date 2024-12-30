01926000b054cadc305b6a288ee876a69a78c806
MOHAMED SESAY <162488876+deeeye2@users.noreply.github.com>
2024-12-30 10:53:39 +0000

    Update HelloWorld.java

--- src/com/example/HelloWorld.java
+++ src/com/example/HelloWorld.java
@@ -3,11 +3,14 @@ package com.example;
 import java.io.IOException;
 import javax.servlet.http.*;
 
-
 public class HelloWorld extends HttpServlet {
+    public String getMessage() {
+        return "Hello, TestNG!";
+    }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         response.getWriter().println("<h1>Hello, World!</h1>");
-        
     }
 }
+
